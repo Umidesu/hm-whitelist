6f5eae4e73d5823e117854c22959ae546ae0597c
Himeni <mrkeysan@gmail.com>
2023-04-11 03:23:08 +0300

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,6 @@ author 'discord himeni#0001'
 description 'Better whitelist script'
 version '1.0.0'
 
-client_scripts {
-    'client.lua',
-}
-
 server_scripts {
     'server.js',
 }
--- server.js
+++ server.js
@@ -6,8 +6,37 @@ Config.role = 'Whitelist role id here'
 const { Client } = require('discord.js')
 const client = new Client({intents:[3276799]})
 
-clğient.on('ready', () => {
+client.on('ready', () => {
+
+    on('playerConnecting', (name, setKickReason, deferrals) => {
+        deferrals.defer()
     
+        const player = global.source;
+    
+        setTimeout(() => {
+            deferrals.update(`Hello ${name}. Your discord ID is being checked.`)
+    
+            let discord_id = null;
+    
+            for (let i = 0; i < GetNumPlayerIdentifiers(player); i++) {
+                const identifier = GetPlayerIdentifier(player, i);
+    
+                if (identifier.includes('discord:')) {
+                    discord_id = identifier;
+                    console.log(discord_id);
+                }
+            }
+    
+            // pretend to be a wait
+            setTimeout(() => {
+                if (discord_id === null) {
+                    deferrals.done("You are not connected to Discord.")
+                } else {
+                    deferrals.done()
+                }
+            }, 0)
+        }, 0)
+    })
 })
 
 client.login(Config.token)
